e31e562cf400c6e241f2ae23b79c847b5adb9d5d
TheBig-C <soymateovera@gmail.com>
2024-03-15 03:42:36 -0400

    arreglo stats

--- lib/user_profile/widget/stats.dart
+++ lib/user_profile/widget/stats.dart
@@ -9,16 +9,16 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 
 class TaskStatisticsWidget extends StatelessWidget {
   final UserModel user;
-   TaskStatisticsWidget({
+  TaskStatisticsWidget({
     super.key,
     required this.user,
   });
-  final CollectionReference tasksCollection = FirebaseFirestore.instance.collection('tasks');
+  final CollectionReference tasksCollection =
+      FirebaseFirestore.instance.collection('tasks');
 
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (context, watch, child) {
-
       return FutureBuilder<List<Task>>(
         future: _fetchFilteredTasks(tasksCollection, user.uid),
         builder: (context, snapshot) {
@@ -29,22 +29,25 @@ class TaskStatisticsWidget extends StatelessWidget {
           } else {
             List<Task> tasks = snapshot.data ?? [];
 
-            Map<TaskCategory, int> categoryCounts = calculateMostUsedCategories(tasks);
-            Map<String, int> completionStatus = calculateCompletionStatus(tasks);
+            Map<TaskCategory, int> categoryCounts =
+                calculateMostUsedCategories(tasks);
+            Map<String, int> completionStatus =
+                calculateCompletionStatus(tasks);
             double averageTimePerTask = calculateAverageTimePerTask(tasks);
-            Map<String, int> taskDistribution = calculateTaskDistributionByDate(tasks);
+            Map<String, int> taskDistribution =
+                calculateTaskDistributionByDate(tasks);
 
             return SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                   Text(
-                              'Estadisticas del usuario: ',
-                              style: const TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
+                  Text(
+                    'Estadisticas del usuario: ',
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   Container(
                     height: 300,
                     padding: EdgeInsets.all(16),
@@ -76,9 +79,13 @@ class TaskStatisticsWidget extends StatelessWidget {
     });
   }
 
-  Future<List<Task>> _fetchFilteredTasks(CollectionReference collection, String userId) async {
-    final querySnapshot = await collection.where('userId', isEqualTo: userId).get();
-    return querySnapshot.docs.map((doc) => Task.fromJson(doc.id, doc.data() as Map<String, dynamic> )).toList();
+  Future<List<Task>> _fetchFilteredTasks(
+      CollectionReference collection, String userId) async {
+    final querySnapshot =
+        await collection.where('userId', isEqualTo: userId).get();
+    return querySnapshot.docs
+        .map((doc) => Task.fromJson(doc.id, doc.data() as Map<String, dynamic>))
+        .toList();
   }
 
   Map<TaskCategory, int> calculateMostUsedCategories(List<Task> tasks) {
@@ -112,91 +119,97 @@ class TaskStatisticsWidget extends StatelessWidget {
       'Pending': pending,
     };
   }
-TimeOfDay _parseTime(String time) {
-  final parts = time.split(':');
-  if (parts.length == 2) {
-    final hour = int.tryParse(parts[0]);
-    final minute = int.tryParse(parts[1]);
-    if (hour != null && minute != null) {
-      return TimeOfDay(hour: hour, minute: minute);
+
+  TimeOfDay _parseTime(String time) {
+    final parts = time.split(':');
+    if (parts.length == 2) {
+      final hour = int.tryParse(parts[0]);
+      final minute = int.tryParse(parts[1]);
+      if (hour != null && minute != null) {
+        return TimeOfDay(hour: hour, minute: minute);
+      }
     }
+    // Si el formato no es válido, devolvemos la hora predeterminada
+    return TimeOfDay.now();
   }
-  // Si el formato no es válido, devolvemos la hora predeterminada
-  return TimeOfDay.now();
-}
 
-double calculateAverageTimePerTask(List<Task> tasks) {
-  if (tasks.isEmpty) return 0.0;
+  double calculateAverageTimePerTask(List<Task> tasks) {
+    if (tasks.isEmpty) return 0.0;
 
-  double totalTime = 0.0;
-  tasks.forEach((task) {
-    final startTime = _parseTime(task.time);
-    final endTime = _parseTime(task.endTime);
+    double totalTime = 0.0;
+    tasks.forEach((task) {
+      final startTime = _parseTime(task.time);
+      final endTime = _parseTime(task.endTime);
 
-    final taskDuration = endTime.hour * 60 +
-        endTime.minute -
-        (startTime.hour * 60 + startTime.minute);
+      final taskDuration = endTime.hour * 60 +
+          endTime.minute -
+          (startTime.hour * 60 + startTime.minute);
 
-    totalTime += taskDuration.toDouble()+30;
-  });
+      totalTime += taskDuration.toDouble() + 30;
+    });
 
-  return totalTime / tasks.length;
-}
-Map<String, int> calculateTaskDistributionByDate(List<Task> tasks) {
-  Map<String, int> distribution = {};
-  tasks.forEach((task) {
-    final taskDate = task.date;
+    return totalTime / tasks.length;
+  }
 
-    distribution.update(
-      taskDate,
-      (value) => value + 1,
-      ifAbsent: () => 1,
+  Map<String, int> calculateTaskDistributionByDate(List<Task> tasks) {
+    Map<String, int> distribution = {};
+    tasks.forEach((task) {
+      final taskDate = task.date;
+
+      distribution.update(
+        taskDate,
+        (value) => value + 1,
+        ifAbsent: () => 1,
+      );
+    });
+    return distribution;
+  }
+
+  Widget mostUsedCategoriesChart(Map<TaskCategory, int> categoryCounts) {
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(),
+      series: <CartesianSeries<TaskCategory, String>>[
+        ColumnSeries<TaskCategory, String>(
+          dataSource: categoryCounts.entries.map((entry) => entry.key).toList(),
+          xValueMapper: (TaskCategory category, _) =>
+              category.toString().substring(13),
+          yValueMapper: (TaskCategory category, _) => categoryCounts[category],
+          dataLabelSettings: DataLabelSettings(
+              isVisible: true), // Mostrar valores encima de las barras
+        ),
+      ],
+      legend: Legend(isVisible: true), // Agregar leyenda al gráfico
     );
-  });
-  return distribution;
-}
-Widget mostUsedCategoriesChart(Map<TaskCategory, int> categoryCounts) {
-  return SfCartesianChart(
-    primaryXAxis: CategoryAxis(),
-    series: <CartesianSeries<TaskCategory, String>>[
-      ColumnSeries<TaskCategory, String>(
-        dataSource: categoryCounts.entries.map((entry) => entry.key).toList(),
-        xValueMapper: (TaskCategory category, _) => category.toString().substring(1),
-        yValueMapper: (TaskCategory category, _) => categoryCounts[category],
-        dataLabelSettings: DataLabelSettings(isVisible: true), // Mostrar valores encima de las barras
-      ),
-    ],
-    legend: Legend(isVisible: true), // Agregar leyenda al gráfico
-  );
-}
+  }
 
-Widget completionStatusChart(Map<String, int> completionStatus) {
-  return SfCircularChart(
-    series: <CircularSeries>[
-      PieSeries<MapEntry<String, int>, String>(
-        dataSource: completionStatus.entries.toList(),
-        xValueMapper: (entry, _) => entry.key,
-        yValueMapper: (entry, _) => entry.value,
-        dataLabelSettings: DataLabelSettings(isVisible: true), // Mostrar valores dentro del pastel
-      ),
-    ],
-    legend: Legend(isVisible: true), // Agregar leyenda al gráfico
-  );
-}
+  Widget completionStatusChart(Map<String, int> completionStatus) {
+    return SfCircularChart(
+      series: <CircularSeries>[
+        PieSeries<MapEntry<String, int>, String>(
+          dataSource: completionStatus.entries.toList(),
+          xValueMapper: (entry, _) => entry.key,
+          yValueMapper: (entry, _) => entry.value,
+          dataLabelSettings: DataLabelSettings(
+              isVisible: true), // Mostrar valores dentro del pastel
+        ),
+      ],
+      legend: Legend(isVisible: true), // Agregar leyenda al gráfico
+    );
+  }
 
-Widget taskDistributionChart(Map<String, int> distribution) {
-  return SfCartesianChart(
-    primaryXAxis: CategoryAxis(),
-    series: <CartesianSeries>[
-      ColumnSeries<MapEntry<String, int>, String>(
-        dataSource: distribution.entries.toList(),
-        xValueMapper: (entry, _) => entry.key,
-        yValueMapper: (entry, _) => entry.value,
-        dataLabelSettings: DataLabelSettings(isVisible: true), // Mostrar valores encima de las barras
-      ),
-    ],
-    legend: Legend(isVisible: true), // Agregar leyenda al gráfico
-  );
-}
-
+  Widget taskDistributionChart(Map<String, int> distribution) {
+    return SfCartesianChart(
+      primaryXAxis: CategoryAxis(),
+      series: <CartesianSeries>[
+        ColumnSeries<MapEntry<String, int>, String>(
+          dataSource: distribution.entries.toList(),
+          xValueMapper: (entry, _) => entry.key,
+          yValueMapper: (entry, _) => entry.value,
+          dataLabelSettings: DataLabelSettings(
+              isVisible: true), // Mostrar valores encima de las barras
+        ),
+      ],
+      legend: Legend(isVisible: true), // Agregar leyenda al gráfico
+    );
+  }
 }
